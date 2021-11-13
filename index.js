2f259e841b471239a40adc263bd4df68a4f8a89e
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2021-11-13 15:59:07 +0300

    created new file index.js and added function to button

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+var year = document.getElementById('year').val();
+var month = document.getElementById('month').val();
+var date = document.getElementById('date').val();
+
+document.getElementById('btn').onclick(function(){
+
+});
