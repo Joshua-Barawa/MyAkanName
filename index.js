8d0a253f36cb7a29a8bf775b184eda66177b64f4
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2021-11-13 16:02:32 +0300

    created person object in index,js file with properites

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
+var person = {
+ year: document.getElementById('year').val(),
+ month: document.getElementById('month').val(),
+ date: document.getElementById('date').val()
 
-var year = document.getElementById('year').val();
-var month = document.getElementById('month').val();
-var date = document.getElementById('date').val();
+}
 
 document.getElementById('btn').onclick(function(){
-
+  console.log()
 });
